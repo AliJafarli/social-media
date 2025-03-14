49d4f2c43885920b2dcec5e0f6300581261239c9
AliJafarli <somagni4icent@gmail.com>
2025-03-15 00:41:34 +0400

    post image entity

---
+++ src/main/java/com/texnoera/socialmedia/model/entity/PostImage.java
@@ -0,0 +1,28 @@
+package com.texnoera.socialmedia.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "post_images")
+public class PostImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "data", length = 1000)
+    private byte[] data;
+}
