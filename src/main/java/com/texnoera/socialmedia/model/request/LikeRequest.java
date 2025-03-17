6c6b86aaaea82f6621f143cfd8ee91ac7048911e
AliJafarli <somagni4icent@gmail.com>
2025-03-17 22:56:49 +0400

    like request

---
+++ src/main/java/com/texnoera/socialmedia/model/request/LikeRequest.java
@@ -0,0 +1,14 @@
+package com.texnoera.socialmedia.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LikeRequest {
+
+    private Long postId;
+    private Long userId;
+}
