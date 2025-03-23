fcb4c60bdf93316efb8abda0c8f2622c74d624d9
AliJafarli <somagni4icent@gmail.com>
2025-03-23 21:44:39 +0400

    userServiceImpl

--- src/main/java/com/texnoera/socialmedia/mapper/UserMapper.java
+++ src/main/java/com/texnoera/socialmedia/mapper/UserMapper.java
@@ -3,11 +3,13 @@ package com.texnoera.socialmedia.mapper;
 import com.texnoera.socialmedia.model.entity.Follow;
 import com.texnoera.socialmedia.model.entity.User;
 import com.texnoera.socialmedia.model.request.UserAddRequest;
+import com.texnoera.socialmedia.model.request.UserUpdateRequest;
 import com.texnoera.socialmedia.model.response.user.UserFollowerResponse;
 import com.texnoera.socialmedia.model.response.user.UserFollowingResponse;
 import com.texnoera.socialmedia.model.response.user.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -17,9 +19,11 @@ public interface UserMapper {
     @Mapping(source = "user.id", target = "userId")
     @Mapping(source = "user.username", target = "username")
     UserFollowerResponse followToFollowerResponse(Follow follow);
+
     @Mapping(source = "following.id", target = "userId")
     @Mapping(source = "following.username", target = "username")
     UserFollowingResponse followToFollowingResponse(Follow follow);
+
     @Mapping(source = "followers", target = "followers")
     @Mapping(source = "following", target = "followings")
     UserResponse userToResponse(User user);
@@ -29,5 +33,8 @@ public interface UserMapper {
     List<UserResponse> usersToResponses(List<User> users);
 
     List<UserFollowingResponse> followsToFollowingResponses(List<Follow> follows);
+
+    void update(@MappingTarget User user, UserUpdateRequest userUpdateRequest);
+
 
 }
---
+++ src/main/java/com/texnoera/socialmedia/model/request/UserUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.texnoera.socialmedia.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateRequest {
+
+    private String password;
+}
