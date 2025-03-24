23bf09c9b2e997f71de58015123525568d4aa203
AliJafarli <somagni4icent@gmail.com>
2025-03-24 21:40:13 +0400

    commentServiceImpl

--- src/main/java/com/texnoera/socialmedia/service/concretes/CommentServiceImpl.java
+++ src/main/java/com/texnoera/socialmedia/service/concretes/CommentServiceImpl.java
@@ -1,8 +1,11 @@
 package com.texnoera.socialmedia.service.concretes;
 
+import com.texnoera.socialmedia.mapper.CommentMapper;
+import com.texnoera.socialmedia.model.entity.Comment;
 import com.texnoera.socialmedia.model.request.CommentAddRequest;
 import com.texnoera.socialmedia.model.request.CommentUpdateRequest;
 import com.texnoera.socialmedia.model.response.comment.CommentGetResponse;
+import com.texnoera.socialmedia.repository.CommentRepository;
 import com.texnoera.socialmedia.service.abstracts.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,38 +15,48 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentMapper commentMapper;
+
     @Override
     public void add(CommentAddRequest commentAddRequest) {
-
+        Comment comment = commentMapper.addRequestToComment(commentAddRequest);
+        commentRepository.save(comment);
     }
 
     @Override
     public List<CommentGetResponse> getAll() {
-        return List.of();
+        List<Comment> comments = commentRepository.findAll();
+        return commentMapper.commentsToResponses(comments);
     }
 
     @Override
     public CommentGetResponse getById(Long id) {
-        return null;
+        Comment comment = commentRepository.findById(id).orElse(null);
+        return commentMapper.commentToResponse(comment);
     }
 
     @Override
     public List<CommentGetResponse> getAllByPost(Long postId) {
-        return List.of();
+        List<Comment> comments = commentRepository.findAllByPost_Id(postId);
+        return commentMapper.commentsToResponses(comments);
     }
 
     @Override
     public List<CommentGetResponse> getAllByUser(Long userId) {
-        return List.of();
+        List<Comment> comments = commentRepository.findAllByUser_Id(userId);
+        return commentMapper.commentsToResponses(comments);
     }
 
     @Override
     public void update(Long id, CommentUpdateRequest commentUpdateRequest) {
 
+        commentRepository.findById(id).ifPresent(commentToUpdate -> commentToUpdate.setCommentText(commentUpdateRequest.getCommentText()));
     }
 
     @Override
     public void delete(Long id) {
-
+        commentRepository.deleteById(id);
     }
 }
