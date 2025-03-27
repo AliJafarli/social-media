3f6d356be35d32af5ebb24f6b9164e235dadf3d6
AliJafarli <somagni4icent@gmail.com>
2025-03-27 23:46:03 +0400

    UserController logs fixed

--- src/main/java/com/texnoera/socialmedia/controller/UserController.java
+++ src/main/java/com/texnoera/socialmedia/controller/UserController.java
@@ -25,37 +25,50 @@ public class UserController {
 
     @GetMapping("/get-all-users")
     public ResponseEntity<List<UserResponse>> getAllUsers() {
-        return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
+        log.info("Received request to get all users");
+        List<UserResponse> users = userService.getAll();
+        log.info("Successfully retrieved {} users", users.size());
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
-        return new ResponseEntity<>(userService.getResponseById(id), HttpStatus.OK);
+        log.info("Received request to get user with ID: {}", id);
+        UserResponse userResponse = userService.getResponseById(id);
+        log.info("Successfully retrieved user with ID: {}", id);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
     @GetMapping("/is-following")
     public ResponseEntity<Boolean> isFollowing(@RequestParam Long userId, @RequestParam Long followingId) {
-        return new ResponseEntity<>(userService.isFollowing(userId, followingId), HttpStatus.OK);
+        log.info("Received request to check if user with ID: {} is following user with ID: {}", userId, followingId);
+        Boolean isFollowing = userService.isFollowing(userId, followingId);
+        log.info("User with ID: {} is following user with ID: {}: {}", userId, followingId, isFollowing);
+        return new ResponseEntity<>(isFollowing, HttpStatus.OK);
     }
 
     @PostMapping("/create-user")
     public ResponseEntity<UserResponse> create(@Valid @RequestBody UserAddRequest userAddRequest) {
         log.info("Received request to add user: {}", userAddRequest.getEmail());
         UserResponse userResponse = userService.add(userAddRequest);
+        log.info("Successfully created user with ID: {}", userResponse.getId());
         return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateRequest userUpdateRequest) {
-        log.info("Updating user with id {}: {}", id, userUpdateRequest);
+        log.info("Received request to update user with ID: {}: {}", id, userUpdateRequest);
         UserResponse userResponse = userService.update(id, userUpdateRequest);
+        log.info("Successfully updated user with ID: {}: {}", id, userResponse);
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.info("Received request to delete user with ID: {}", id);
         userService.delete(id);
+        log.info("Successfully deleted user with ID: {}", id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
