487e94c0184e7ebbd0f46a9903e827daf665a55c
AliJafarli <somagni4icent@gmail.com>
2025-03-28 21:38:18 +0400

    UserImageController added

---
+++ src/main/java/com/texnoera/socialmedia/controller/UserImageController.java
@@ -0,0 +1,45 @@
+package com.texnoera.socialmedia.controller;
+
+
+import com.texnoera.socialmedia.model.response.userImage.UserImageResponse;
+import com.texnoera.socialmedia.service.abstracts.UserImageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Log4j2
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user-images")
+public class UserImageController {
+
+    private final UserImageService userImageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<UserImageResponse> upload(@RequestParam("image") MultipartFile file, @RequestParam Long userId) throws IOException {
+        log.info("Received request to upload image for userId={}", userId);
+        log.info("Uploading file: {} for userId={} with size={} bytes",
+                file.getOriginalFilename(), userId, file.getSize());
+        UserImageResponse response = userImageService.upload(file, userId);
+        log.info("Successfully uploaded file for userId={}", userId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{userId}")
+    public ResponseEntity<byte[]> download(@PathVariable Long userId) {
+        log.info("Received request to download image for userId={}", userId);
+        byte[] image = userImageService.download(userId);
+        log.info("Successfully retrieved image for userId={}", userId);
+        return ResponseEntity.status(HttpStatus.OK).
+                contentType(MediaType.IMAGE_PNG).
+                body(image);
+    }
+}
