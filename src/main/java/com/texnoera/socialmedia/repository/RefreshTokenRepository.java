527e5d5b7590e869e381431721948461498a6ab3
AliJafarli <somagni4icent@gmail.com>
2025-04-04 19:42:19 +0400

    refresh token repository + service added

---
+++ src/main/java/com/texnoera/socialmedia/repository/RefreshTokenRepository.java
@@ -0,0 +1,15 @@
+package com.texnoera.socialmedia.repository;
+
+import com.texnoera.socialmedia.model.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    Optional<RefreshToken> findByUserId(Long userId);
+}
---
+++ src/main/java/com/texnoera/socialmedia/service/concretes/RefreshTokenServiceImpl.java
@@ -0,0 +1,36 @@
+package com.texnoera.socialmedia.service.concretes;
+
+import com.texnoera.socialmedia.model.entity.RefreshToken;
+import com.texnoera.socialmedia.model.entity.User;
+import com.texnoera.socialmedia.repository.RefreshTokenRepository;
+import com.texnoera.socialmedia.service.abstracts.RefreshTokenService;
+import com.texnoera.socialmedia.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenServiceImpl implements RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Override
+    public RefreshToken generateOrUpdateRefreshToken(User user) {
+        return refreshTokenRepository.findByUserId(user.getId())
+                .map(refreshToken -> {
+                    refreshToken.setCreated(LocalDateTime.now());
+                    refreshToken.setToken(ApiUtils.generateUuidWithoutDash());
+                    return refreshTokenRepository.save(refreshToken);
+                })
+                .orElseGet(() -> {
+                    RefreshToken newToken = new RefreshToken();
+                    newToken.setUser(user);
+                    newToken.setCreated(LocalDateTime.now());
+                    newToken.setToken(ApiUtils.generateUuidWithoutDash());
+                    return refreshTokenRepository.save(newToken);
+                });
+    }
+}
